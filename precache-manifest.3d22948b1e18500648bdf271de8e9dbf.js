083a2172f279da3aa01d4c60770caced6c2eceb9
Felipe Lima <felipe_lima10@icloud.com>
2021-09-15 13:08:33 -0300

    Updates

---
+++ precache-manifest.3d22948b1e18500648bdf271de8e9dbf.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d7bd0964b6f366625865ee0c7f4c2a",
+    "url": "/project-trivia/index.html"
+  },
+  {
+    "revision": "cbfb681148f360ae2ae0",
+    "url": "/project-trivia/static/css/main.2949dfc4.chunk.css"
+  },
+  {
+    "revision": "a31511646f3d2ee51da2",
+    "url": "/project-trivia/static/js/2.b0cd5357.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/project-trivia/static/js/2.b0cd5357.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbfb681148f360ae2ae0",
+    "url": "/project-trivia/static/js/main.2f55a67c.chunk.js"
+  },
+  {
+    "revision": "cc34cd09b747f1081c99",
+    "url": "/project-trivia/static/js/runtime-main.d9e70092.js"
+  },
+  {
+    "revision": "d202c301d4f66ebf901cc788107fa732",
+    "url": "/project-trivia/static/media/0.d202c301.svg"
+  },
+  {
+    "revision": "d9ce77c9ede038257bfc9e81c5c651ad",
+    "url": "/project-trivia/static/media/1.d9ce77c9.svg"
+  },
+  {
+    "revision": "d3d99502f039f9846fed068306973dac",
+    "url": "/project-trivia/static/media/2.d3d99502.svg"
+  },
+  {
+    "revision": "5d322e64f62081dd2451501e50c8c5b6",
+    "url": "/project-trivia/static/media/brain_1.5d322e64.png"
+  },
+  {
+    "revision": "4c3b6e13df47eb1c13a40df41c51271f",
+    "url": "/project-trivia/static/media/brain_2.4c3b6e13.png"
+  },
+  {
+    "revision": "cb6edcd63cc80bbb4c1e4df0cd1ade84",
+    "url": "/project-trivia/static/media/config.cb6edcd6.svg"
+  },
+  {
+    "revision": "e48c2700eacf3e5bcc05d92272cb35f2",
+    "url": "/project-trivia/static/media/header.e48c2700.svg"
+  },
+  {
+    "revision": "6f564c6c9af69f94e283e5c759ad70dd",
+    "url": "/project-trivia/static/media/home.6f564c6c.svg"
+  },
+  {
+    "revision": "989f7fe9f60129927e16365f2ed5a90f",
+    "url": "/project-trivia/static/media/music.989f7fe9.svg"
+  },
+  {
+    "revision": "2823b103f0c40147ff10c0fff38d7e23",
+    "url": "/project-trivia/static/media/sneaky.2823b103.mp3"
+  },
+  {
+    "revision": "31882b3e33bbe537e6531d4209f47a1b",
+    "url": "/project-trivia/static/media/stop.31882b3e.svg"
+  },
+  {
+    "revision": "e323612ae5849e6510da1f7cecccbcd2",
+    "url": "/project-trivia/static/media/trivia.e323612a.png"
+  },
+  {
+    "revision": "840ae09f9b1e91e73fb54745e90d2f9d",
+    "url": "/project-trivia/static/media/warning.840ae09f.svg"
+  },
+  {
+    "revision": "bda62d85ef254c7ba174bc9e76426bfa",
+    "url": "/project-trivia/static/media/world_1.bda62d85.png"
+  },
+  {
+    "revision": "ecb3974b50ea64f86a4a50e7823ee1d6",
+    "url": "/project-trivia/static/media/world_2.ecb3974b.png"
+  },
+  {
+    "revision": "e00a0778e799d3388b36eac1914073cd",
+    "url": "/project-trivia/static/media/x.e00a0778.svg"
+  }
+]);
